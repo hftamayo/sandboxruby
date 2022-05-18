fd434fa8945be5b6de0b5470f23179fdbab1ab70
hftamayo <hftamayo@gmail.com>
2022-05-18 15:59:50 -0600

    hackerrank_intro_exercises_part1

---
+++ foundations/hackerank/foundations/e02_array_init.rb
@@ -0,0 +1,14 @@
+=begin
+One of the most commonly used data structures in Ruby is a Ruby Array, and below we see various methods of initializing a ruby array.
+
+Your task is to initialize three different variables as explained below.
+
+    Initialize an empty array with the variable name array
+
+
+=end
+
+# Initialize 3 variables here as explained in the problem statement
+array = []
+array_1=[nil]
+array_2=[10,10]
