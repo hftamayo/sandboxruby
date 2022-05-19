09b08d4e3df3de8fcd30d624ac0da5a172008fc7
hftamayo <hftamayo@gmail.com>
2022-05-19 15:47:22 -0600

    hackerrank_intro_exercises_part2

--- foundations/hackerank/foundations/e11_hashes.rb
+++ foundations/hackerank/foundations/e11_hashes.rb
@@ -3,7 +3,7 @@ power_function = -> (x, z) {
 }
 
 base = gets.to_i
-raise_to_power = power_function.curry.(2)
+raise_to_power = power_function.curry.(base)
 
 power = gets.to_i
 puts raise_to_power.(power)
