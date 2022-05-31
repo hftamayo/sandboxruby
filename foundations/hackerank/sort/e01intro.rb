74aa699aaf4f0772ae4df82045822af1fa90b975
hftamayo <hftamayo@gmail.com>
2022-05-31 10:11:04 -0600

    sorting: intro

---
+++ foundations/hackerank/sort/e01intro.rb
@@ -0,0 +1,15 @@
+=begin
+This is a simple challenge to get things started. Given a sorted array () and a number (), can you print the index location of
+in the array?
+
+Example
+arr=[1,2,3]
+v=3
+
+Return 2 for a zero-based index array. 
+=end
+
+def introTutorial(v, arr)
+    # Write your code here
+    p arr.index(v)
+end
